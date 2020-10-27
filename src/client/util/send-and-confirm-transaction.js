ee0f481ee65aa9c07463a733791e3b848f485fc7
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-10-27 20:09:44 +0100

    rip out more dead code and deps

--- src/client/util/send-and-confirm-transaction.js
+++ src/client/util/send-and-confirm-transaction.js
@@ -1,11 +1,4 @@
 import {sendAndConfirmTransaction as realSendAndConfirmTransaction} from '@solana/web3.js';
-import YAML from 'json-to-pretty-yaml';
-
-let notify = () => undefined;
-
-export function onTransaction(callback) {
-  notify = callback;
-}
 
 export async function sendAndConfirmTransaction(
   title,
@@ -13,30 +6,14 @@ export async function sendAndConfirmTransaction(
   transaction,
   ...signers
 ) {
-  const when = Date.now();
-
-  const signature = await realSendAndConfirmTransaction(
+  await realSendAndConfirmTransaction(
     connection,
     transaction,
     signers,
     {
       skipPreflight: true,
-      commitment: 'recent',
+      commitment: 'singleGossip',
       preflightCommitment: null,
     },
   );
-
-  const body = {
-    time: new Date(when).toString(),
-    signature,
-    instructions: transaction.instructions.map(i => {
-      return {
-        keys: i.keys.map(keyObj => keyObj.pubkey.toBase58()),
-        programId: i.programId.toBase58(),
-        data: '0x' + i.data.toString('hex'),
-      };
-    }),
-  };
-
-  notify(title, YAML.stringify(body).replace(/"/g, ''));
 }
