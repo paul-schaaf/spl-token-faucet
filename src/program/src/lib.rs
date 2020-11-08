4038af525fa7d3fc4a73b4d7491adf03c9aee0aa
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-08 21:17:56 +0100

    throw out escrow out, create simple pool program

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -1,5 +1,8 @@
-pub mod entrypoint;
-pub mod error;
-pub mod instruction;
-pub mod processor;
-pub mod state;
+use serum_pool::{declare_pool_entrypoint, Pool};
+
+enum SimplePool {}
+
+impl Pool for SimplePool {}
+
+#[cfg(not(feature = "no-entrypoint"))]
+declare_pool_entrypoint!(SimplePool);
